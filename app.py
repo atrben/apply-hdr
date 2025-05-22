e56edd9f495a995b9ca9fb4eb453b05d615bfdf0
atrben <142738440+atrben@users.noreply.github.com>
2025-05-22 11:15:07 -0700

    Update app.py

--- app.py
+++ app.py
@@ -112,4 +112,6 @@ def download_file(filename):
     return send_from_directory(app.config['PROCESSED_FOLDER'], filename, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port) 
